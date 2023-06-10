6638d2d47156057233e707c489cca243df606327
Leo Merlo <merloleandro@gmail.com>
2023-06-10 11:27:05 -0300

    first run

--- tp2/backend/server.js
+++ tp2/backend/server.js
@@ -2,6 +2,9 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+app.use('/api', express.json())
+
 app.listen(2023, function () {
   console.log('FoodGenie API -> http://localhost:2023')
 })
